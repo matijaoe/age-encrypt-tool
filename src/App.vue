725ea330bd4388f90c9d2947781191da9983a516
Matija Osrečki <github@matijao.com>
2025-01-11 12:09:31 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <UApp>
+    <header>
+      <div class="wrapper">
+        <HelloWorld msg="You did it!" />
+      </div>
+    </header>
+  </UApp>
+</template>
